6c4ac554ab4158fcb134ed06ccb229e8da07fed6
Dishant-20 <dishantrajput2020@gmail.com>
2024-03-22 16:37:24 +0530

    22-MARCH-2024:4:37pm

---
+++ IL-Ricercatore/controllers/screens/chart_monitors/add_blood_pressure/add_blood_pressure.swift
@@ -0,0 +1,29 @@
+//
+//  add_blood_pressure.swift
+//  IL-Ricercatore
+//
+//  Created by Dishant Rajput on 22/03/24.
+//
+
+import UIKit
+
+class add_blood_pressure: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ IL-Ricercatore/controllers/screens/chart_monitors/blood_pressure.swift
@@ -0,0 +1,16 @@
+//
+//  blood_pressure.swift
+//  IL-Ricercatore
+//
+//  Created by Dishant Rajput on 22/03/24.
+//
+
+import UIKit
+
+class blood_pressure: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
