0d45e3ce396713e28544198c5470b6995f63df5b
Dishant-20 <dishantrajput2020@gmail.com>
2024-05-08 19:37:49 +0530

    8-may-2024:7:37pm

---
+++ IL-Ricercatore/controllers/screens/reminders/walk_reminder/walk_reminder.swift
@@ -0,0 +1,29 @@
+//
+//  walk_reminder.swift
+//  IL-Ricercatore
+//
+//  Created by Dishant Rajput on 08/05/24.
+//
+
+import UIKit
+
+class walk_reminder: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
